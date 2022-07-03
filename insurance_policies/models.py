01103e51bb69e66bf67b5b4dd371be38fdf3a81f
aleks <alekseykrylov@mail.ru>
2022-07-03 15:14:11 +0300

    Add Django Model InsurancePolicy,Client,Product,TypeOfInsurance,InsuranceCompany, Region

--- insurance_policies/models.py
+++ insurance_policies/models.py
@@ -1,3 +1,90 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Client(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField("Name", max_length=255)
+    first_name = models.CharField("First name", max_length=50)
+    middle_name = models.CharField("Middle name", max_length=50)
+    last_name = models.CharField("Last name", max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField("Name", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class TypeOfInsurance(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField("Name", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class InsuranceCompany(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField("Name", max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+# InsurancePolicy
+# -
+# InsurancePolicyID PK int
+# Number string INDEX
+# PolicyholderID int FK >- Client.ClientID
+# PolicyownerID int FK >- Client.ClientID
+# ProductID int FK >- Product.ProductID
+# InsuranceCompanyID int FK >- InsuranceCompany.InsuranceCompanyID
+# TypeOfInsuranceID int FK >- TypeOfInsurance.TypeOfInsuranceID
+# RegionID int FK >- Region.RegionID
+# RegistrationDate DateTime
+# StartDate DateTime
+# EndDate DateTime
+# InsuranceSum money
+# InsurancePremium money
+
+class Region(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField("Name", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class InsurancePolicy(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    number = models.CharField("Number", max_length=100, unique=True)
+    policy_holder = models.OneToOneField(Client, on_delete=models.CASCADE)
+    policy_owner = models.OneToOneField(Client, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    insurance_company = models.OneToOneField(InsuranceCompany, on_delete=models.CASCADE)
+    type_Of_insurance = models.OneToOneField(TypeOfInsurance, on_delete=models.CASCADE)
+    region = models.OneToOneField(Region, on_delete=models.CASCADE)
+    registration_date = models.DateTimeField("Registration date", auto_now_add=True)  # https://docs.djangoproject.com/en/4.0/ref/models/fields/#datetimefield
+    start_date = models.DateTimeField('Start date', auto_now_add=True)
+    end_date = models.DateTimeField('End date', auto_now_add=False)
+    # duration = models.DurationField('Duration', )
+    insurance_sum = models.DecimalField(..., max_digits=15, decimal_places=2)
+    insurance_premium = models.DecimalField(..., max_digits=15, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+
+    def __str__(self):
+        return self.number
