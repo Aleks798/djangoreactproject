b0a7ffeed8893c8f7262219238269d4ab12ef33f
aleks <alekseykrylov@mail.ru>
2022-07-04 22:08:36 +0300

    Change InsurancePolicy, add classes RiskTable, PeriodOfInsurance change fields insurance_sum, insurance_premium in the class InsurancePolicy

--- insurance_policies/models.py
+++ insurance_policies/models.py
@@ -44,6 +44,14 @@ class InsuranceCompany(models.Model):
         return self.name
 
 
+class Risk(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField("Name", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 # InsurancePolicy
 # -
 # InsurancePolicyID PK int
@@ -77,14 +85,36 @@ class InsurancePolicy(models.Model):
     insurance_company = models.ForeignKey(InsuranceCompany, on_delete=models.DO_NOTHING)
     type_Of_insurance = models.ForeignKey(TypeOfInsurance, on_delete=models.DO_NOTHING)
     region = models.ForeignKey(Region, on_delete=models.DO_NOTHING)
-    registration_date = models.DateTimeField("Registration date", auto_now_add=True)  # https://docs.djangoproject.com/en/4.0/ref/models/fields/#datetimefield
+    registration_date = models.DateTimeField("Registration date",
+                                             auto_now_add=True)  # https://docs.djangoproject.com/en/4.0/ref/models/fields/#datetimefield
     start_date = models.DateTimeField('Start date', auto_now_add=True)
     end_date = models.DateTimeField('End date', auto_now_add=False)
     # duration = models.DurationField('Duration', )
-    insurance_sum = models.DecimalField(..., max_digits=15, decimal_places=2)
-    insurance_premium = models.DecimalField(..., max_digits=15, decimal_places=2)
+    insurance_sum = models.DecimalField(max_digits=15, decimal_places=2)
+    insurance_premium = models.DecimalField(max_digits=15, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
 
     def __str__(self):
         return self.number
+
+
+class RiskTable(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    insurance_policy = models.ForeignKey(InsurancePolicy, on_delete=models.DO_NOTHING, related_name='insurance_policy')
+    risk = models.ForeignKey(Risk, on_delete=models.DO_NOTHING, related_name='risk')
+    insurance_rate = models.DecimalField(max_digits=7, decimal_places=4)
+    insurance_premium = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return self.id
+
+
+class PeriodOfInsurance(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    insurance_policy = models.ForeignKey(InsurancePolicy, on_delete=models.DO_NOTHING, related_name='insurance_policy')
+    start_date = models.DateTimeField('Start date', auto_now_add=True)
+    end_date = models.DateTimeField('End date', auto_now_add=False)
+
+    def __str__(self):
+        return self.id
