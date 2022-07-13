10824929661450c2afe850ba31f4f6c6c8f80830
aleks <alekseykrylov@mail.ru>
2022-07-13 21:01:20 +0300

    Change AXIOS API_URL

--- frontend/src/ClientsService.js
+++ frontend/src/ClientsService.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000';
+const API_URL = 'http://192.168.56.102:8000';
 
 export default class ClientsService{
 
