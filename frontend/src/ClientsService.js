036f09d0eca41ca1c5d15eaa19403d07e8ea2617
aleks <alekseykrylov@mail.ru>
2022-07-19 22:15:19 +0300

    changed headers in createClient(), updateClient() v.10

--- frontend/src/ClientsService.js
+++ frontend/src/ClientsService.js
@@ -26,18 +26,19 @@ export default class ClientsService{
 	createClient(client){
 		const url = `${API_URL}/api/clients/`;
 		const config = {headers: {"Access-Control-Allow-Origin": "*"}};
+		axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 		return axios.post(url, client, config);
 	}
 	updateClient(client){
 		const url = `${API_URL}/api/clients/${client.pk}`;
-		console.log('v.7 updateClient(client): url ='+url);
+		console.log('v.10 updateClient(client): url ='+url);
 		//console.log(client);
 		//const config = {headers: {"Access-Control-Allow-Origin": "*"}};
-		//axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-		const headers =  {
-			'Access-Control-Allow-Origin': '*',
-		};
-		return axios.put(url, client, {headers});
-		//return axios.put(url, client);
+		axios.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
+		// const headers =  {
+		// 	'Access-Control-Allow-Origin': '*',
+		// };
+		//return axios.put(url, client, {headers});
+		return axios.put(url, client);
 	}
 }
