2f76b8c100e6f025491c39bd00d022e43b8fef7c
aleks <alekseykrylov@mail.ru>
2022-07-19 23:02:19 +0300

    added CORS_ORIGIN_ALLOW_ALL v.4

--- djangoreactproject/settings.py
+++ djangoreactproject/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
 #     'http://192.168.56.102:3000/*',
 # ]
 
-# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 # CORS_ALLOW_CREDENTIALS = True
 
 
