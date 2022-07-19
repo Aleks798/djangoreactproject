ca324df0a326fc94d1f120f2c59890f24504e782
aleks <alekseykrylov@mail.ru>
2022-07-19 19:40:11 +0300

    changed CORS_ORIGIN_WHITELIST

--- djangoreactproject/settings.py
+++ djangoreactproject/settings.py
@@ -147,7 +147,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
        'https://localhost:3000',
+        'https://192.168.56.102:3000',
 )
