b0fcff11ea8f230f0ff7778699a74b3a61ad249e
aleks <alekseykrylov@mail.ru>
2022-07-20 16:48:45 +0300

    Added urls, views, Serializer for Products

--- insurance_policies/views.py
+++ insurance_policies/views.py
@@ -4,8 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import InsurancePolicy
-from .models import Client
+from .models import InsurancePolicy, Client, Product
 from .serializers import *
 
 
@@ -135,4 +134,66 @@ def clients_detail(request, pk):
 
     elif request.method == 'DELETE':
         client.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def products_list(request):
+    """
+ List of products, or create a new product.
+ """
+    if request.method == 'GET':
+        data = []
+        nextPage = 1
+        previousPage = 1
+        products = Product.objects.all()
+        page = request.GET.get('page', 1)
+        paginator = Paginator(products, 10)
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
+
+        serializer = ProductSerializer(data, context={'request': request}, many=True)
+        if data.has_next():
+            nextPage = data.next_page_number()
+        if data.has_previous():
+            previousPage = data.previous_page_number()
+
+        return Response({'data': serializer.data, 'count': paginator.count, 'numpages': paginator.num_pages,
+                         'nextlink': '/api/products/?page=' + str(nextPage),
+                         'prevlink': '/api/products/?page=' + str(previousPage)})
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def products_detail(request, pk):
+    """
+    Retrieve, update or delete a product by id/pk.
+    """
+    try:
+
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
